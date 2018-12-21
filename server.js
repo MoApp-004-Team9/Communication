4a00957a8106f07ace06915a23e127f98633f142
min103jun <min103jun@naver.com>
2018-12-21 19:34:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,41 @@ app.get('/', function () {
      console.log('connect');
 });
 
+var SerialPort = require('serialport'),
+    portName = 'COM5',//포트 번호 확인 후 추가
+    port = new SerialPort(portName),
+    SerialPort = 0;
+
+port.on('open', function () {
+    console.log('Serial Port OPEN');
+
+});
+
 io.on('connection', function (socket) {
     console.log('user connection.');
 
-    socket.on('lightOn', function (data) {
-     
+    socket.on('LightOn', function (data) {
+        console.log('LightOn');
+        port.write(1);
+    })
+    socket.on('LightOff', function (data) {
+        console.log('LightOff');
+        port.write(2);
+    })
+    socket.on('AirOn', function (data) {
+        console.log('AirOn');
+        port.write(3);
+    })
+    socket.on('AirOff', function (data) {
+        console.log('AirOff');
+        port.write(4);
+    })
+    socket.on('BoilerOn', function (data) {
+        console.log('BoilerOn');
+        port.write(5);
+    })
+    socket.on('BoilerOff', function (data) {
+        console.log('BoilerOff');
+        port.write(6);
     })
 });
